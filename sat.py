de98c4bd5e7ed57e5cfca40e0403e4e41f39f7ac
nparzan <parzan@gmail.com>
2020-01-01 10:16:16 +0200

    Initial commit

---
+++ sat.py
@@ -0,0 +1,145 @@
+import random
+
+
+class Clause:
+
+    def __init__(self, mask_pos, mask_neg, n):
+        # Gets two numbers from [0,...,2^n - 1],
+        # Interprets the "1" bits in the first as positive variables
+        # And the "1" bits in the second as negative variables
+        self.vars_pos = mask_pos
+        self.vars_neg = mask_neg
+        self.len = n
+        self.tautology = False
+        # Tautology
+        if mask_pos & mask_neg:
+            mask_pos = mask_neg = pow(2, n) - 1
+            self.tautology = True
+
+    def __len__(self):
+        return self.len
+
+    def __neg__(self):
+        # Given a clause, returns the "opposite clause",
+        # I.e., map x_i to !(x_i)
+        return Clause(self.vars_neg, self.vars_pos, self.len)
+
+    def __repr__(self):
+        if self.tautology:
+            return "1"
+        s_pos = bin(self.vars_pos)[2:]
+        s_neg = bin(self.vars_neg)[2:]
+        st = ""
+        for i in range(self.len):
+            if len(s_pos) > i and s_pos[len(s_pos) - i - 1] == '1':
+                st += "x" + str(i) + ", "
+            if len(s_neg) > i and s_neg[len(s_neg) - i - 1] == '1':
+                st += "-x" + str(i) + ", "
+        return st[:-2]
+
+    def __call__(self, assignment_pos, assignment_neg):
+        # Gets two numbers from [0,...,2^n - 1],
+        # Interprets the "1" bits in the first as positive variables
+        # And the "1" bits in the second as negative variables
+        pos = assignment_pos & self.vars_pos
+        neg = assignment_neg & self.vars_neg
+        return bool(pos or neg)
+
+    @staticmethod
+    def random(n):
+        # Draw a random 3-variable clause over n variables
+        bits = set()
+        while len(bits) != 3:
+            bits = set(random.randint(0, n - 1) for i in range(3))
+        mask_pos = 0
+        mask_neg = 0
+        for bit in bits:
+            if random.randint(0, 1) == 1:
+                mask_pos += pow(2, bit)
+            else:
+                mask_neg += pow(2, bit)
+        cl = Clause(mask_pos, mask_neg, n)
+        return cl
+
+
+class Formula:
+
+    def __init__(self, clauses):
+        # Gets a sequence of clauses and defines them as the formula
+        self.clauses = [clause for clause in clauses]
+        self.nof_vars = 0
+
+        if self.clauses:
+            self.nof_vars = len(self.clauses[0])
+
+    @staticmethod
+    def random(n, k):
+        # Draw a random 3CNF formula over n variables with k clauses
+        cl = []
+        for i in range(k):
+            cl += [Clause.random(n)]
+        return Formula(cl)
+
+    @staticmethod
+    def random_assignment(n):
+        # Draw a random assignment over n variables
+        # We draw a random positive assignment and
+        # The negative assignment is the complement
+        pos = random.randint(0, 2**n - 1)
+        neg = 2**n - 1 - pos
+        return (pos, neg)
+
+    def __repr__(self):
+        st = ""
+        for cl in self:
+            st += str(cl) + " and "
+        return st[:-5]
+
+    def __iter__(self):
+        self.iter_idx = 0
+        return self
+
+    def __len__(self):
+        return len(self.clauses)
+
+    def __getitem__(self, i):
+        assert 0 <= i < len(self)
+
+        return self.clauses[i]
+
+    def __next__(self):
+        # Iteration method simply iterates over all clauses
+        if self.iter_idx < len(self):
+            res = self[self.iter_idx]
+            self.iter_idx += 1
+            return res
+        raise StopIteration
+
+    def __call__(self, assignment_pos, assignment_neg):
+        # Evaluate a formula with a given assignment
+        # If any clause is unsatisfied return False
+        # Else, return True
+        for clause in self:
+            if not clause(assignment_pos, assignment_neg):
+                return False
+        return True
+
+    def brute_force(self, count=False):
+        # Find a solution using naive brute force search
+        # If count=True, count all solutions
+        counter = 0
+        start = 0
+        end = 2**(self.nof_vars) - 1
+
+        while start <= end:
+            if self(start, end):
+                if not count:
+                    return (start, end)
+                counter += 1
+
+            start += 1
+            end -= 1
+
+        if count:
+            return counter
+        return None
---
+++ usage.py
@@ -0,0 +1,44 @@
+from sat import *
+
+n = 10
+# for i in range(100):
+#     cl1 = Clause.random(n)
+#     print(cl1)
+
+# m_p = 2**9
+# m_n = 2**4 + 2**8
+# c1 = (Clause(m_p, m_n, 10))
+# print(c1)
+# print(c1(2**9 + 2**4, 0))
+# print("ABC")
+# F = Formula([c1, -c1])
+
+# for c in F:
+#     print(c)
+
+# print(F(2**9 + 2**4, 0))
+
+G = Formula.random(10, 5)
+ass = Formula.random_assignment(10)
+
+# print(G(ass[0], ass[1]))
+# print(Clause(ass[0], ass[1], 10))
+# print(bin(ass[0]), bin(ass[1]))
+# print(G)
+
+L = Formula.random(20, 50)
+sol = L.brute_force()
+if sol:
+    print(Clause(sol[0], sol[1], 20))
+
+print(L.brute_force(count=True) / (2**20))
+
+approx = 0
+k = 10000
+for i in range(k):
+    ass = Formula.random_assignment(20)
+    if (L(ass[0], ass[1])):
+        print(Clause(ass[0], ass[1], 20))
+        approx += 1
+
+print("Acceptance probability:", approx / k)
